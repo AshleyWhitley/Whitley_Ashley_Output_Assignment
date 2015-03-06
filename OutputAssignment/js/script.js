23209fc169ee28f7576f90e6a4d9de2a133c113a
Ashley Whitley <paintrevelations@gmail.com>
2015-03-05 21:39:41 -0800

    Alerts
    Updated titles to different codes and added an alert

--- OutputAssignment/js/script.js
+++ OutputAssignment/js/script.js
@@ -1,7 +1,16 @@
 /**
  * Created by MissyGotGame on 3/5/15.
  */
+// Value and Data Types
 
 var favoriteNumber = 7;
 
+// Printing to the Console
+
 console.log(favoriteNumber);
+
+// Alerts
+
+alert("Using Alerts!")
+
+
